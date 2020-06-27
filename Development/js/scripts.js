3f2fe15913257f88b6a19e132996c8d582f03de7
abdullahalzaabi <66646611+abdullahalzaabi@users.noreply.github.com>
2020-06-27 20:43:30 +0300

    تم حل التمرين

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,24 +1,31 @@
 // السؤال الأول
 // 1
-
+const name = "Abdullah"
 // 2
+const Number = 16;
 
 // 3 and 4
-
+console.log( name + "" + Number)
 // 5 and 6
-
+console.log (number *5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies {"singing". "walking" . "drawing" }
+name=javascript
 // 2
-
+hobbies.push
 // 3
-
+hobbies.pop
 // 4
-
+let object1 ={
+    web 
+    html + css
+أ.حسين | أ.رهف | أ.موضي | أ.مناور
+}
 // 5
+
 
 // 6
 
